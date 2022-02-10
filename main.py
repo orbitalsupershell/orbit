80a72289b96821e93f46869255e7ad1d42ed0dcb
fortress <73935250+orbitalsupershell@users.noreply.github.com>
2022-02-10 17:38:51 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+
+for x in range(0,5000):
+    c = x
+
+    ans = c^3 #Enter 
+    anscondition = 4*(c+2)  # Enter condition of answer here
+    if ans == anscondition:
+        print(f"Found {c}!!!")
+
+
+# 2*(c+11) = 4*(c+2)
